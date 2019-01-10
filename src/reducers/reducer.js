d5c235d35965279af6914ee62917d3a88098c70a
bidachen <39844391+bidachen@users.noreply.github.com>
2019-01-09 21:57:49 -0500

    Add files via upload

---
+++ src/reducers/reducer.js
@@ -0,0 +1,76 @@
+import { combineReducers } from "redux";
+import {
+  ADD_ITEM,
+  SELECT_TABLE,
+  ACTIVE_TABLE,
+  DELETE_ITEM,
+  INCREASE_MONEY
+} from "../constants/constants.js";
+
+let tableList = [];
+for (let i = 0; i < 16; i++) {
+  tableList.push([]);
+}
+let tableStatus = new Array(16).fill(false);
+
+const tableItems = (state = tableList, action) => {
+  switch (action.type) {
+    case ADD_ITEM:
+      var newState = [...state];
+      newState[action.tableID].push(action.item);
+      return newState;
+    case DELETE_ITEM:
+      var newState = [];
+      for (let i = 0; i < 16; i++) {
+        if (i !== action.tableID) newState.push([...state[i]]);
+        else
+          newState.push([
+            ...state[i].slice(0, action.index),
+            ...state[i].slice(action.index + 1)
+          ]);
+      }
+      return newState;
+    case ACTIVE_TABLE:
+      var newState = [...state];
+      newState[action.tableID] = [];
+      return newState;
+    default:
+      return state;
+  }
+};
+const selectTable = (state = null, action) => {
+  switch (action.type) {
+    case SELECT_TABLE:
+      if (state === action.tableID) return null;
+      return action.tableID;
+    default:
+      return state;
+  }
+};
+
+const activeTable = (state = tableStatus, action) => {
+  switch (action.type) {
+    case ACTIVE_TABLE:
+      var newState = [...state];
+      newState[action.tableID] = !newState[action.tableID];
+      return newState;
+    default:
+      return state;
+  }
+};
+
+const totalMoney = (state = 0, action) => {
+  switch (action.type) {
+    case INCREASE_MONEY:
+      return state + action.amount;
+    default:
+      return state;
+  }
+};
+const reducer = combineReducers({
+  selectTable,
+  tableItems,
+  activeTable,
+  totalMoney
+});
+export default reducer;
