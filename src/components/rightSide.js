513de16d33de284a3677e9a887144a9b38903ff5
bidachen <39844391+bidachen@users.noreply.github.com>
2019-01-12 20:44:25 -0500

    Create rightSide.js

---
+++ src/components/rightSide.js
@@ -0,0 +1,14 @@
+import { Jumbotron, Button, Container, Row, Col } from "reactstrap";
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+const RightSide = props => {
+  return (
+    <Col className="right" xs="2">
+      <Button />
+    </Col>
+  );
+};
+
+export default RightSide;
