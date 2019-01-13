2d9b3058d4d3715ce6b6eca39ed11bb8e675e346
bidachen <39844391+bidachen@users.noreply.github.com>
2019-01-12 20:43:27 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,27 @@
-import { Jumbotron, Button } from "reactstrap";
+import { Jumbotron, Button, Container, Row, Col } from "reactstrap";
 import React from "react";
+
 import "bootstrap/dist/css/bootstrap.css";
 
 const Header = props => {
+  console.log(props);
   return (
     <div>
-      <Jumbotron>
-        <h1 className="display-3">My restaurent </h1>
-        <p className="lead">table avaible:</p>
-        <p className="lead">money earned:</p>
-      </Jumbotron>
+      <Row>
+        <Col>
+          <h1>My restaurent</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h4>table avaible: {props.totalMoney}</h4>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h4>money earned:</h4>
+        </Col>
+      </Row>
     </div>
   );
 };
