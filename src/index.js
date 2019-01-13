f64edad1bc8b17ea6e61b819a717007412e1ded7
bidachen <39844391+bidachen@users.noreply.github.com>
2019-01-12 20:42:56 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import LeftSide from "./components/leftSide.js";
+import App from "./components/app.js";
 import "bootstrap/dist/css/bootstrap.css";
 import "./styles.css";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers/reducer.js";
+import addItem from "./actions/addItem.js";
+import deleteItem from "./actions/deleteItem.js";
+import selectTable from "./actions/selectTable.js";
+import activeTable from "./actions/activeTable.js";
+import increaseMoney from "./actions/increaseMoney.js";
+var store = createStore(reducer);
+
+store.dispatch(activeTable(10));
+store.dispatch(addItem(10, "1", 20));
+store.dispatch(activeTable(10));
+console.log(store.getState());
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<LeftSide />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
