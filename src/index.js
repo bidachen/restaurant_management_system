9ca8a2a5628b89aad50de8c099e896bc8af23e07
bidachen <39844391+bidachen@users.noreply.github.com>
2019-01-13 21:46:32 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,17 +6,7 @@ import "./styles.css";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers/reducer.js";
-import addItem from "./actions/addItem.js";
-import deleteItem from "./actions/deleteItem.js";
-import selectTable from "./actions/selectTable.js";
-import activeTable from "./actions/activeTable.js";
-import increaseMoney from "./actions/increaseMoney.js";
 var store = createStore(reducer);
-
-store.dispatch(activeTable(10));
-store.dispatch(addItem(10, "1", 20));
-store.dispatch(activeTable(10));
-console.log(store.getState());
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
