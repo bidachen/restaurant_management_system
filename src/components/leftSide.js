62bb346a4b3f4a653972b05283a94bc0303dc4bf
bidachen <39844391+bidachen@users.noreply.github.com>
2019-01-13 21:54:53 -0500

    Update leftSide.js

--- src/components/leftSide.js
+++ src/components/leftSide.js
@@ -25,7 +25,9 @@ const LeftSide = props => {
       );
     });
   }
-
+  let check = "Check In";
+  if (props.selectTable && props.activeTable[props.selectTable])
+    check = "Check Out";
   return (
     <Col className="left" xs="2">
       <h3>Table #: {props.selectTable}</h3>
@@ -33,7 +35,7 @@ const LeftSide = props => {
         color="success"
         onClick={() => props.onActive(props.selectTable, totalBill)}
       >
-        check
+        {check}
       </Button>
       <h5>Bill total: {totalBill}</h5>
       <h6>Orders: {orders}</h6>
