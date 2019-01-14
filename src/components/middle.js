079166f878b27a93bea38c6f91caa5ccb2a07dca
bidachen <39844391+bidachen@users.noreply.github.com>
2019-01-13 21:44:41 -0500

    Update middle.js

--- src/components/middle.js
+++ src/components/middle.js
@@ -1,4 +1,4 @@
-import { Jumbotron, Button, Container, Row, Col } from "reactstrap";
+import { Button, Col } from "reactstrap";
 import React from "react";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -6,7 +6,18 @@ import "bootstrap/dist/css/bootstrap.css";
 const Middle = props => {
   let buttons = [];
   for (let i = 0; i < 16; i++)
-    buttons.push(<Button color="info">Table {i}</Button>);
+    buttons.push(
+      <Button
+        color="info"
+        id={i}
+        key={i}
+        onClick={event => {
+          props.onSelect(event.target.id);
+        }}
+      >
+        Table {i}
+      </Button>
+    );
   return (
     <Col className="middle" xs="8">
       {buttons}
