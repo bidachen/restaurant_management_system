1b0b538db4f7913f8947a9f06c00475160748beb
bidachen <39844391+bidachen@users.noreply.github.com>
2019-01-13 21:44:28 -0500

    Update leftSide.js

--- src/components/leftSide.js
+++ src/components/leftSide.js
@@ -1,15 +1,42 @@
-import { Jumbotron, Button, Container, Row, Col } from "reactstrap";
+import { Button, Col } from "reactstrap";
 import React from "react";
 
 import "bootstrap/dist/css/bootstrap.css";
 
 const LeftSide = props => {
+  let totalBill = 0;
+  let orders = [];
+  if (props.selectTable) {
+    orders = props.tableItems[props.selectTable].map((item, index) => {
+      totalBill += item.price;
+      return (
+        <div>
+          <Button
+            id={index}
+            key={index}
+            onClick={event => {
+              props.onDelete(props.selectTable, event.target.id);
+            }}
+          >
+            X
+          </Button>
+          {item.name} | ${item.price}
+        </div>
+      );
+    });
+  }
+
   return (
     <Col className="left" xs="2">
-      <h3>Table #:</h3>
-      <Button color="success"> check in</Button>
-      <h5>Bill total: </h5>
-      <h6>Orders:</h6>
+      <h3>Table #: {props.selectTable}</h3>
+      <Button
+        color="success"
+        onClick={() => props.onActive(props.selectTable, totalBill)}
+      >
+        check
+      </Button>
+      <h5>Bill total: {totalBill}</h5>
+      <h6>Orders: {orders}</h6>
     </Col>
   );
 };
